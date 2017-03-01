41af33cbb0aab794d596f52fb7bbe6caf1fd499d
Chijioke Ndubisi <cjndubisi@gmail.com>
2017-03-01 19:13:17 +0100

    Rename files

--- JitenshaMobile/APIClinet/JitenshaAPI.swift
+++ JitenshaMobile/APIClinet/JitenshaAPI.swift
@@ -1,92 +1,71 @@
 //
-//  JitenshaAPI.swift
+//  Auth.swift
 //  JitenshaMobile
 //
 //  Created by Chijioke Ndubisi on 01/03/2017.
 //
 //
 
-import SwiftyJSON
 import PromiseKit
+import SwiftyJSON
 import Moya
+import MoyaSugar
 
-protocol Auth {
-    var bToken: String! {get set}
-
-    func signUp(with email: String, password: String ) -> Promise<Void>
-    func login(with email: String, password: String) -> Promise<Void>
-    func places() -> Promise<[JSON]>
+enum Jitensha {
+    case signup(String, String)
+    case login(String, String)
+    case places
+    case rent([String:Any])
 }
 
-class APIClinet: Auth {
+extension Jitensha: SugarTargetType {
 
-    var bToken: String!
-    static let shared = APIClinet()
-
-    private init() {
-
-    }
-
-    func login(with email: String, password: String) -> Promise<Void> {
-        return Promise{ fulfill, reject in
-            MoyaProvider<Jitensha>()
-                .request(.login(email, password)) { (result) in
-
-                    switch result {
-                    case let .success(response):
-                        do {
-                            let json = try response.mapJSON()
-                            self.bToken = JSON(json)["accessToken"].string
-                            fulfill()
-                        } catch {
-                            reject(error)
-                        }
-                    case let .failure(error):
-                        reject(error)
-                    }
-            }
+    var route: MoyaSugar.Route {
+        switch self {
+        case .signup:
+            return .post("/register")
+        case .login:
+            return .post("/auth")
+        case .places:
+            return .get("/places")
+        case .rent:
+            return .post("/rent")
         }
     }
 
-    func signUp(with email: String, password: String) -> Promise<Void> {
-        return Promise{ fulfill, reject in
-            MoyaProvider<Jitensha>()
-                .request(.signup(email, password)) { (result) in
-
-                    switch result {
-                    case let .success(response):
-                        do {
-                            let json = try response.mapJSON()
-                            self.bToken = JSON(json)["accessToken"].string
-                            fulfill()
-                        } catch {
-                            reject(error)
-                        }
-                    case let .failure(error):
-                        reject(error)
-                    }
-            }
+    var params: MoyaSugar.Parameters? {
+        switch self {
+        case let .signup(email, password):
+            return JSONEncoding() => ["username":email, "password":password]
+        case let .login(email, password):
+            return JSONEncoding() => ["username":email, "password":password]
+        case .places:
+            return nil
+        case let .rent(card):
+            return JSONEncoding() => card
         }
     }
 
-    func places() -> Promise<[JSON]> {
-        return Promise { fulfill, reject in
-            MoyaProvider<Jitensha>()
-                .request(Jitensha.places) { (result) in
-                    switch result {
-                    case let .success(response):
-                        do {
-                            let json = try response.mapJSON()
-                            let places = JSON(json)["result"].array!
-                            fulfill(places)
-                        } catch {
-                            reject(NSError(domain: "JSON Mapping", code: 300, userInfo: [NSLocalizedDescriptionKey: "Could not complete operation"]))
-                        }
-                    case let .failure(error):
-                        reject(error)
-                    }
+    var httpHeaderFields: [String : String]? {
+        return self.token == nil ? nil : ["Authorization": self.token]
+    }
 
-                }
-        }
+    var task: Task {
+        return .request
+    }
+}
+
+extension SugarTargetType {
+
+    var baseURL: URL {
+        return URL(string: "https://localhost:8080/api/v1/auth")!
+    }
+
+    var token: String! {
+        return AppDelegate.delegate.apiToken
+    }
+
+    var sampleData: Data {
+        return Data()
     }
 }
